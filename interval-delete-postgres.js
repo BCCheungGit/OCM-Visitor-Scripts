b10cf26d4bacb76e231aae2c1cebff008a396ae2
BCCheungGit <bc3431@nyu.edu>
2024-07-24 23:14:42 -0400

    script now deletes all old visitors every day at midnight 00:00

--- interval-delete-postgres.js
+++ interval-delete-postgres.js
@@ -7,16 +7,6 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 })
 
-pool.connect()
-    .then(() => {
-        console.log('connected to Postgres on port: 5432');
-        deleteOldVisitors();
-    })
-    .catch((err) => {
-        console.error('connection error', err.stack);
-    })
-
-
 
 async function deleteOldVisitors() {
     try {
@@ -24,26 +14,42 @@ async function deleteOldVisitors() {
             secretKey: process.env.CLERK_SECRET_KEY
         })
         const users = await clerk.users.getUserList();
-        const userIds = users.data.filter(user => Object.keys(user.publicMetadata).length === 0)
-        .map(user => user.id);
+        const userIds = users.data.filter(user => Object.keys(user.publicMetadata).length === 0).map(user => user.id);
         console.log(userIds);
         
         for (i = 0; i < userIds.length; i++) {
             await clerk.users.deleteUser(userIds[i]);
+            console.log(`Deleted user with id: ${userIds[i]}`);
         }
         
-        const queryString = `DELETE FROM "ocm-visitors_visitors" WHERE user_id in (${userIds.map((id, index) => `'${id}'${index < userIds.length - 1 ? ',' : ''}`).join('')})`;
-        pool.query(queryString, (err, res) => {
-            if (err) {
-                console.error(err.stack);
-            } else {
-                console.log(res.rows);
-            }
-        }
-        
-    );
+        const queryString = `DELETE FROM "ocm-visitors_visitors" WHERE user_id IN (${userIds.map((id, index) => `$${index + 1}`).join(', ')})`;
+
+        const params = userIds.map(id => id); 
+
+        const result = await pool.query(queryString, params);
+        console.log(result);    
+    
 
     } catch (error) {
         console.log(error);
     }
 }
+
+pool.connect()
+    .then(() => {
+        console.log('connected to Postgres on port: 5432');
+        cron.schedule('0 0 * * *', async () => {
+            try {
+                await deleteOldVisitors();
+                console.log('Successfully deleted old visitors');
+            } catch (error) {
+                console.error('Cron job error:', error);
+            }
+        });
+
+    })
+    .catch((err) => {
+        console.error('connection error', err.stack);
+    })
+
+
